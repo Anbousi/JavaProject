ab7bf60c9c0f2c3a902c058b4228b6545d0c6dfc
lana yaghi <yaghilana.alaa@gmail.com>
2021-07-03 13:33:11 +0300

    Static files were added

--- JavaProject/src/main/java/com/axsosacademy/demo/config/WebSecurityConfig.java
+++ JavaProject/src/main/java/com/axsosacademy/demo/config/WebSecurityConfig.java
@@ -32,14 +32,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	    protected void configure(HttpSecurity http) throws Exception {
 	        http.
 	            authorizeRequests()
-	                .antMatchers("/css/**", "/js/**", "/registration","/category*/painting*","/").permitAll()
+	                .antMatchers("/css/**", "/js/**", "/registration","/category*","/category*/painting*","/home").permitAll()
 	                .antMatchers("/admin/**").hasRole("ADMIN")
 	                .anyRequest().authenticated()
 	                .and()
 	            .formLogin()
 	                .loginPage("/login")
 	                .permitAll()
-//	                .defaultSuccessUrl("/home", true)
+                .defaultSuccessUrl("/home", true)
 	                .and()
 	            .logout()
 	                .permitAll();
