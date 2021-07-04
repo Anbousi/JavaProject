6ecb25ced53f3d3083a3ebd9cdeea3c6e3450679
mhmdhabibessa <mhmdhabib1999@gmail.com>
2021-07-04 22:18:42 +0300

    actepted

--- JavaProject/src/main/java/com/axsosacademy/demo/repositories/PaintingRepository.java
+++ JavaProject/src/main/java/com/axsosacademy/demo/repositories/PaintingRepository.java
@@ -1,5 +1,7 @@
 package com.axsosacademy.demo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
